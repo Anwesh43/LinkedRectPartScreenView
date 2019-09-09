593c5564064c3087f3d58c90b675f4f85b965f4c
Anwesh43 <anweshthecool0@gmail.com>
2019-09-10 02:00:28 +0530

    adding RectPartScreenView class

--- rectpartscreenview/src/main/java/com/anwesh/uiprojects/rectpartscreenview/RectPartScreenView.kt
+++ rectpartscreenview/src/main/java/com/anwesh/uiprojects/rectpartscreenview/RectPartScreenView.kt
@@ -54,3 +54,21 @@ fun Canvas.drawRPSNode(i : Int, scale : Float, sc : Float, currI : Int, paint : 
     drawRectParts(scale, sc, w, hSize, i == currI, paint)
     restore()
 }
+
+class RectPartScreenView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                
+            }
+        }
+        return true
+    }
+}
