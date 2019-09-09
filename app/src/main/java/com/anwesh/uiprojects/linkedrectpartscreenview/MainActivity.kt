77b5113febbf4573486a6e8cbbc9201098de7cc3
Anwesh43 <anweshthecool0@gmail.com>
2019-09-10 02:25:24 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedrectpartscreenview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrectpartscreenview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.linkedrectpartscreenview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.rectpartscreenview.RectPartScreenView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        RectPartScreenView.create(this)
     }
 }
